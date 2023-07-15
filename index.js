786d6d86fa922db7232750fe6da03efdc0c2d81b
AJ Javadi <amirjavadi25@gmail.com>
2023-07-14 20:28:05 -0500

    index server and connection working

---
+++ index.js
@@ -0,0 +1,23 @@
+// set up all of our variables
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+const PORT = process.env.PORT || 3001;
+// set up variable for database connection
+const db = mongoose.connection;
+// const seedUsers = require('./utils/seed');
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+
+// connect to the database
+db.once('open', async () =>  {
+    console.log('Connected to the database.');
+    // await seedUsers();
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+}
+);
---
+++ utils/seed.js
@@ -0,0 +1,1 @@
+// TODO: create a function that bulk creates users using the User model
