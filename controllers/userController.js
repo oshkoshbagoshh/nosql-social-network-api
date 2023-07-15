857543d25e81c87cbc4de5ab5c4c800954191525
AJ Javadi <amirjavadi25@gmail.com>
2023-07-15 00:01:50 -0500

    fixing error with user controller

---
+++ controllers/userController.js
@@ -0,0 +1,12 @@
+// set up controllers in this file to handle user requests
+//
+// // import the models
+const { Users, Thoughts } = require('../models');
+
+// set up the user controller
+module.exports = {
+    // get all users
+    async getAllUsers(req, res) {
+        try {
+            const userData = await Users.find().populate('thoughts').populate('friends');
+        }
