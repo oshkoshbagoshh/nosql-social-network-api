4df23d79d4bd62e4a08b37ad434ec6956fb47820
AJ Javadi <amirjavadi25@gmail.com>
2023-07-27 09:56:43 -0500

    final commit

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,6 @@
 const { User } = require('../models');
 const connection = require('../config/connection');
+const { Thought } = require('../models');
 
 
 // seed data
@@ -38,20 +39,41 @@ const users = [
 
 ];
 
+
+
+// seed data for thoughts
+// const { Thought } = require('../models');
+const thoughts = [
+    
+    {
+      thoughtText: 'this is a test thought',
+      username: 'lernantino',
+      reactions: ['this is a test reaction', 'this is another test reaction'],
+    },
+    {
+      thoughtText: 'this is another test thought',
+      username: 'ajavadi',
+      reactions: [],
+    }
+  ];
+
+
+
 // connect to server
 connection.once('open', async () => {
   // delete all data from the database
   await User.deleteMany({})
   // bulk create users 
   await User.collection.insertMany(users);
-
+  
+  
+  // delete thoughts 
+  await Thought.deleteMany({})
+  //bulk create thoughts
+  await Thought.collection.insertMany(thoughts);
   // log the data
   console.table(users); // log the data
+  console.table(thoughts); // log the data
   console.log('all done with seeding'); // log that the script is done
   process.exit(0); // exit the script
 });
-
-
-// run the script
-
-
